61c2189e2f4131602c846b948f18a3d7d0cae30d
talhasuhayeb <talhasuhayeb@gmail.com>
2024-09-25 07:33:49 +0600

    test commit

--- app.js
+++ app.js
@@ -4,18 +4,20 @@ const donationButton1 = document.getElementById("donation1");
 donationButton1.addEventListener("click", function () {
   const amount1 = parseInt(document.getElementById("amountInput1").value);
   const noakhaliDonation = document.getElementById("donationAmount1");
-  noakhaliDonation.innerText = `${amount1} BDT`;
+  if (amount1 > 0) {
+    noakhaliDonation.innerText = `${amount1} BDT`;
+  }
+
+  if (amount1 <= 0 || isNaN(amount1)) {
+    alert("Invalid Donation Amount ");
+    return;
+  }
 
   let initialBalance = 5500;
   totalDonation += amount1;
   const remainingBalence = initialBalance - totalDonation;
   const balanceElement = document.getElementById("remainBalence");
   balanceElement.innerText = `${remainingBalence} BDT`;
-
-  if (amount1 <= 0 || isNaN(amount1)) {
-    alert("Invalid Donation Amount ");
-    return;
-  }
 
   if (amount1 > remainingBalence) {
     alert("Insufficient balance.");
@@ -36,7 +38,14 @@ const donationButton2 = document.getElementById("donation2");
 donationButton2.addEventListener("click", function () {
   const amount2 = parseInt(document.getElementById("amountInput2").value);
   const feniDonation = document.getElementById("donationAmount2");
-  feniDonation.innerText = `${amount2} BDT`;
+  if (amount2 > 0) {
+    feniDonation.innerText = `${amount2} BDT`;
+  }
+
+  if (amount2 <= 0 || isNaN(amount2)) {
+    alert("Invalid Donation Amount ");
+    return;
+  }
 
   let initialBalance = 5500;
   totalDonation += amount2;
@@ -44,10 +53,6 @@ donationButton2.addEventListener("click", function () {
   const balanceElement = document.getElementById("remainBalence");
   balanceElement.innerText = `${remainingBalence} BDT`;
 
-  if (amount2 <= 0 || isNaN(amount2)) {
-    alert("Invalid Donation Amount ");
-    return;
-  }
   if (amount2 > remainingBalence) {
     alert("Insufficient balance.");
     return;
@@ -67,7 +72,14 @@ const donationButton3 = document.getElementById("donation3");
 donationButton3.addEventListener("click", function () {
   const amount3 = parseInt(document.getElementById("amountInput3").value);
   const quotaDonation = document.getElementById("donationAmount3");
-  quotaDonation.innerText = `${amount3} BDT`;
+  if (amount3 > 0) {
+    quotaDonation.innerText = `${amount3} BDT`;
+  }
+
+  if (amount3 <= 0 || isNaN(amount3)) {
+    alert("Invalid Donation Amount ");
+    return;
+  }
 
   let initialBalance = 5500;
   totalDonation += amount3;
@@ -75,10 +87,6 @@ donationButton3.addEventListener("click", function () {
   const balanceElement = document.getElementById("remainBalence");
   balanceElement.innerText = `${remainingBalence} BDT`;
 
-  if (amount3 <= 0 || isNaN(amount3)) {
-    alert("Invalid Donation Amount ");
-    return;
-  }
   if (amount3 > remainingBalence) {
     alert("Insufficient balance.");
     return;
@@ -129,4 +137,7 @@ function updateHistory() {
     div.innerText = item;
     historyDiv.appendChild(div);
   });
+}
+function displayDate() {
+  document.getElementById("showDate").innerHTML = new Date().appendChild("p");
 }
